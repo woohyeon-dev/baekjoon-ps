2b0fe38536ce1896fc82856f3f45ae6a1ba6bb4b
woohyeon-dev <97883403+woohyeon-dev@users.noreply.github.com>
2022-10-21 11:21:53 +0900

    [Bronze V] Title: N 찍기, Time: 176 ms, Memory: 17004 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= N; i++) {
+      sb.append(i).append("\n");
+    }
+    System.out.println(sb);
+  }
+}
