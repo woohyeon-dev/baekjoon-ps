0bc919753aa23c63842b04b4bc8584a0639196ec
woohyeon-dev <97883403+woohyeon-dev@users.noreply.github.com>
2022-10-28 11:44:10 +0900

    [Bronze III] Title: 부호, Time: 704 ms, Memory: 123228 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    for (int i = 0; i < 3; i++) {
+      int n = Integer.parseInt(br.readLine());
+      BigInteger sum = BigInteger.ZERO;
+      for (int j = 0; j < n; j++) {
+        sum = sum.add(new BigInteger(br.readLine()));
+      }
+
+      // compareTo(0)이 -1이면 음수, 1이면 양수, 0이면 0
+      if (sum.compareTo(BigInteger.ZERO) == 0) {
+        System.out.println(0);
+      } else if (sum.compareTo(BigInteger.ZERO) == 1) {
+        System.out.println("+");
+      } else if (sum.compareTo(BigInteger.ZERO) == -1) {
+        System.out.println("-");
+      }
+    }
+  }
+}
